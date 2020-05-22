e0ba0cba51ce801d7430c444c96ce355729aad21
Prashant-jumpbyte <prashant.b@appgambit.com>
2020-05-22 13:23:45 +0530

    update code build base image

--- modules/codebuild/Main.tf
+++ modules/codebuild/Main.tf
@@ -56,7 +56,7 @@ resource "aws_codebuild_project" "codebuild_project_1" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:2.0"
+    image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
